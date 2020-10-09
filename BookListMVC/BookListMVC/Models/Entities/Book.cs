27736bd85814efde50d903ddb6548598fbd5074e
Adriano Alves da Silva <58754259+adrianoalves-cwb@users.noreply.github.com>
2020-10-08 23:26:57 -0300

    First BookList commit

---
+++ BookListMVC/BookListMVC/Models/Entities/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookListMVC.Models
+{
+    [Table("Book")]
+    public class Book
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+}
